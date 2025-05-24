7878159f66290f2f5e57773611f5ba1a08a4f0a9
Aryan0412 <aryanyadav6678@gmail.com>
2025-05-24 15:21:37 +0530

    clerkauth setup, homepage for the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,41 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import Link from "next/link";
+import { LogIn } from "lucide-react";
 
-export default function Home() {
+export default async function Home() {
+
+  const { userId } = await auth();
+  const isAuth = !!userId;
   return (
-    <div><h1 className="text-red-300">Hello World!</h1>
-      <Button  >Click me</Button></div>
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex item-center" >
+            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <UserButton />
+          </div>
+          <div className="flex mt-2 ">
+            {
+              isAuth && (
+                <Button className="bg-black hover:bg-black/80 text-white cursor-pointer">Go to Chats</Button>
+              )
+            }
+          </div>
+          <p className="max-w-xl mt-1 text-lg text-slate-600">Join millions of students and professionals to instantly answer questions and understand research with AI</p>
+          <div className="w-full mt-4">
+            {
+              isAuth ? (<h1>File Upload</h1>) : <Link href={'/sign-in'} >
+                <Button>Login to get started
+                  <LogIn className="w-4 h-4 ml-2" />
+                </Button>
+              </Link>
+            }
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }

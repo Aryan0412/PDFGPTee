5b6325412a12174e3755f2656f4d4525379678a5
Aryan0412 <aryanyadav6678@gmail.com>
2025-05-27 08:32:22 +0530

    db schema, setting up neon db and drizzle orm, file-upload component

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+// configuration file to config drizzle with the schema and database url
+
+import type {Config} from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+dotenv.config({path : '.env'});
+
+export default {
+    dialect : 'postgresql',
+    schema : './src/lib/db/schema.ts',
+    dbCredentials : {
+        url : process.env.DATABASE_URL!
+    }
+} satisfies Config;
---
+++ src/components/FileUpload.tsx
@@ -0,0 +1,28 @@
+'use client'
+import { Inbox } from 'lucide-react';
+import React from 'react'
+import {useDropzone} from 'react-dropzone';
+const FileUpload = () => {
+    const {getRootProps, getInputProps} = useDropzone({
+        accept : {'application/pdf' : ['.pdf']},
+        maxFiles : 1,
+        onDrop : (acceptedFiles) => {
+            console.log(acceptedFiles);
+        }
+    });
+  return (
+    <div className='p-2 bg-white rounded-xl' >
+        <div {...getRootProps({
+            className : 'border-2 border-dashed rounded-xl p-8 cursor-pointer bg-gray-50 flex justify-center items-center flex-col'
+        })}>
+            <input {...getInputProps()}  />
+            <>
+                <Inbox className='w-10 h-10 text-blue-500' />
+                <p className='mt-2 text-sm text-slate-400'>Drop PDF here or click to upload</p>
+            </>
+        </div>
+    </div>
+  )
+}
+
+export default FileUpload

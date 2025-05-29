dac7dc4c9f5f7c0a947feed7f09c237ed075766f
Aryan0412 <aryanyadav6678@gmail.com>
2025-05-29 10:09:58 +0530

    drizzle configuration, orm schema for chats and pdf list

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,4 +1,3 @@
-import { timeStamp } from 'console';
 import {integer, pgEnum, pgTable, serial, text, timestamp, varchar} from 'drizzle-orm/pg-core'
 
 export const userSystemEnum = pgEnum('user_system_enum', ['system', 'user']);
